c34bb18ffcea72a0efe525fab08e68994589b3c0
Sopow <Sopowbs@gmail.com>
2024-07-28 18:25:23 +0200

    add type as a keyword

--- web/src/powlang-grammar.ts
+++ web/src/powlang-grammar.ts
@@ -1,5 +1,5 @@
 const powLangGrammar: any = {
-  keyword: /\b(?:define|number|string|show|when|as)\b/,
+  keyword: /\b(?:define|number|string|show|when|as|type)\b/,
   boolean: /\b(?:true|false)\b/,
   operator: /=e|=s|=i|[+*\/><=-]/,
   number: /\b\d+\b/,
