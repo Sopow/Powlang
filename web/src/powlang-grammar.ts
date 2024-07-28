57a6e538d0cfdbb97ef1a111603c4f9c7e62c92a
Sopow <Sopowbs@gmail.com>
2024-07-28 18:57:25 +0200

    grammar

--- web/src/powlang-grammar.ts
+++ web/src/powlang-grammar.ts
@@ -1,5 +1,5 @@
 const powLangGrammar: any = {
-  keyword: /\b(?:define|number|string|show|when|as|type)\b/,
+  keyword: /\b(?:define|number|string|show|when|as|type|ala|otw)\b/,
   boolean: /\b(?:true|false)\b/,
   operator: /=e|=s|=i|[+*\/><=-]/,
   number: /\b\d+\b/,
